506ff5642e42f343234f5536002f286096e29dc7
Asadulla56 <mdasadullahoque294@gmail.com>
2025-04-19 17:48:18 +0600

    change the color button

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
    <div className="flex flex-col justify-center items-center h-screen gap-4">
     <h1 className="text-4xl font-bold text-blue-300">This is our DevCabin</h1>
     <p>This is a cabin for developers</p>
-    <button className="bg-blue-300 text-white px-4 py-2 rounded-md">developing...</button>
+    <button className="bg-blue-500 text-white px-4 py-2 rounded-md">developing...</button>
    </div>
 
    </>
